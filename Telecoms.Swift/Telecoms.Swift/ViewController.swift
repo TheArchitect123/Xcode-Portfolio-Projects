3540205c0aee50f52f356f60586c6bc941e2f243
Dan Gerchcovich <dan.developer789@gmail.com>
2020-05-27 15:43:25 +1000

    Telecoms Project
    Initial Commit

---
+++ Telecoms.Swift/Telecoms.Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telecoms.Swift
+//
+//  Created by Assassin on 27/5/20.
+//  Copyright © 2020 Dan Gerchcovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Telecoms.Swift/Telecoms.SwiftTests/Telecoms_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Telecoms_SwiftTests.swift
+//  Telecoms.SwiftTests
+//
+//  Created by Assassin on 27/5/20.
+//  Copyright © 2020 Dan Gerchcovich. All rights reserved.
+//
+
+import XCTest
+@testable import Telecoms_Swift
+
+class Telecoms_SwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
