2029157c2be1ab374f03ef43df4c252a364a08b7
Dan Gerchcovich <dan.developer789@gmail.com>
2020-06-13 21:16:02 +1000

    Fixed Issues With Arbitrary Loads

--- SWAPIConsumer.Swift/SWAPIConsumer.Swift/Constants/UrlConstants.swift
+++ SWAPIConsumer.Swift/SWAPIConsumer.Swift/Constants/UrlConstants.swift
@@ -11,12 +11,5 @@ import Foundation
 
 struct UrlConstants {
     static let Remote_BaseUrl = "https://swapi.dev/api/";
-    static let Remote_BaseUrlForPeople = "https://swapi.dev/api/people";
-    static let Remote_BaseUrlForPlanets = "https://swapi.dev/api/planets";
-    static let Remote_BaseUrlForFilms = "https://swapi.dev/api/films";
-    static let Remote_BaseUrlForSpecies = "https://swapi.dev/api/species";
-    static let Remote_BaseUrlVehicles = "https://swapi.dev/api/vehicles";
-    static let Remote_BaseUrlStarships = "https://swapi.dev/api/starships";
-    
     static let Remote_BaseUrl_ForTesting = "http://";
 }
