5415354f07167deafb8e6bd1737598acf267f1dd
Dan Gerchcovich <dan.developer789@gmail.com>
2020-12-16 00:47:30 +1100

    Update DashboardActivity.java

--- ABCNews/app/src/main/java/com/securebox/abcnewsdroid/activities/dashboard/DashboardActivity.java
+++ ABCNews/app/src/main/java/com/securebox/abcnewsdroid/activities/dashboard/DashboardActivity.java
@@ -21,6 +21,7 @@ public class DashboardActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
