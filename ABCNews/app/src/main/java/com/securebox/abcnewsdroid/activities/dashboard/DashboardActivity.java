602ac3d593545b05e6f6332ca67f4ea4fc69ce53
Dan Gerchcovich <dan.developer789@gmail.com>
2020-12-22 22:17:41 +1100

    ABC News App -- Dashboard Update

--- ABCNews/app/src/main/java/com/securebox/abcnewsdroid/activities/dashboard/DashboardActivity.java
+++ ABCNews/app/src/main/java/com/securebox/abcnewsdroid/activities/dashboard/DashboardActivity.java
@@ -8,9 +8,14 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.provider.MediaStore;
 import android.view.View;
 
 import com.securebox.abcnewsdroid.R;
+
+import java.io.File;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -20,6 +25,7 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         
         FloatingActionButton fab = findViewById(R.id.fab);
