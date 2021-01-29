1c7eb9d86d8cfa744c06d549a1a2f276207bc78a
Dan Gerchcovich <dan.developer789@gmail.com>
2021-01-30 01:08:14 +1100

    Reset & Change Password Fragments

--- NoteBook/app/src/main/java/com/securebox/notebook/components/activities/authentication/signIn/fragments/FragmentRegisterComponent.java
+++ NoteBook/app/src/main/java/com/securebox/notebook/components/activities/authentication/signIn/fragments/FragmentRegisterComponent.java
@@ -24,15 +24,6 @@ public class FragmentRegisterComponent extends BaseCompatFragment<FragmentRegist
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_register_component.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentRegisterComponent newInstance(String param1, String param2) {
         FragmentRegisterComponent fragment = new FragmentRegisterComponent();
         Bundle args = new Bundle();
